4439bfcef11fc84e54bd4e48126fc81ae33e5ef8
Magandalf <56162834+Magandalf@users.noreply.github.com>
2020-02-01 13:25:19 +0300

    fix

--- common/defines/autism_defines.lua
+++ common/defines/autism_defines.lua
@@ -68,9 +68,8 @@ NDefines.NProduction.EQUIPMENT_MODULE_ADD_XP_COST = 0.0					-- XP cost for addin
 NDefines.NProduction.EQUIPMENT_MODULE_REPLACE_XP_COST = 0.0				-- XP cost for replacing one equipment module with an unrelated module when creating an equipment variant.
 NDefines.NProduction.EQUIPMENT_MODULE_CONVERT_XP_COST = 0.0				-- XP cost for converting one equipment module to a related module when creating an equipment variant.
 NDefines.NProduction.EQUIPMENT_MODULE_REMOVE_XP_COST = 0.0				-- XP cost for removing an equipment module and leaving the slot empty when creating an equipment variant.
-NDefines.NProduction.BASE_LICENSE_IC_COST = 0,							-- Base IC cost for lended license
-NDefines.NProduction.LICENSE_IC_COST_YEAR_INCREASE = 0,
-
+NDefines.NProduction.BASE_LICENSE_IC_COST = 0 						-- Base IC cost for lended license
+NDefines.NProduction.LICENSE_IC_COST_YEAR_INCREASE = 0
 
 ---Military
 NDefines.NMilitary.RECON_SKILL_IMPACT = 8
